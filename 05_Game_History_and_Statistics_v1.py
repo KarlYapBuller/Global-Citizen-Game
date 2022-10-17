b111a4ec27bcc7b63f45cb7362603ede3a719116
KarlYapBuller <Karl.yap@Buller.ac.nz>
2022-10-18 04:35:16 +1300

    #Completed Game History and Statistics version 1

--- 05_Game_History_and_Statistics_v1.py
+++ 05_Game_History_and_Statistics_v1.py
@@ -5,8 +5,12 @@ game_summary = []
 
 #Questions answered correct
 questions_answered_correct = 0
+
 #Questions answered incorrect
 questions_answered_incorrect = 0
+
+#The number of question answered
+number_of_questions_answered = 5
 
 #Loop of 5 questions
 for question_number in range(0, 5):
@@ -29,9 +33,14 @@ for question_number in range(0, 5):
     #Puts the question outcome in the game summary list
     game_summary.append(question_outcome)
 
+#Calculate Game Statistics
+percent_correct = questions_answered_correct / number_of_questions_answered * 100
+percent_incorrect = questions_answered_incorrect / number_of_questions_answered * 100
+
 #The game summary is displayed to the User
 #The game sumary includes the Question number and the question answered result
 #The number of questions answered correct and incorrect is displayed to the User
+#The percentage of how many questions they have answered correct and how many questions they have answered incorrect
 print()
 print("*****Game History*****")
 for game in game_summary:
@@ -39,4 +48,7 @@ for game in game_summary:
 
 print()
 
-print("Questions Answered Correct: {} \t|\t Questions Answered Incorrectly: {} \t".format(questions_answered_correct, questions_answered_incorrect))
+print("Questions Answered Correct: {} ({:.0f}%) \t|\t Questions Answered Incorrectly: {} ({:.0f}%) \t".format(questions_answered_correct,
+                                                                                                              percent_correct,
+                                                                                                              questions_answered_incorrect,
+                                                                                                              percent_incorrect))
