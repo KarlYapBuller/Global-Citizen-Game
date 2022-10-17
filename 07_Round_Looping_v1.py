39c7d7978ce0a58826df3d06bb2fb48e4561d650
KarlYapBuller <Karl.yap@Buller.ac.nz>
2022-10-18 04:34:15 +1300

    #Completed Round Looping Version 1

--- 07_Round_Looping_v1.py
+++ 07_Round_Looping_v1.py
@@ -302,6 +302,8 @@ while game_loop == "":
 
     if game_mode == 1:
 
+        print("Game Mode: Leviathan")
+
         while user_health and leviathan_health != 0:
 
             # Iterate through the dictionary (quiz_questions_and_answers) to get the key, value pair and also the index (used for the Question number)
@@ -367,6 +369,8 @@ while game_loop == "":
 
     if game_mode == 2:
 
+        print("Game Mode: Kraken")
+
         while user_health and kraken_health != 0:
 
             # Iterate through the dictionary (quiz_questions_and_answers) to get the key, value pair and also the index (used for the Question number)
@@ -431,6 +435,8 @@ while game_loop == "":
         break
 
     if game_mode == 3:
+
+        print("Game Mode: Hydra")
 
         while user_health and hydra_health != 0:
 
